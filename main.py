8f33567f82b3e7caa196fd8a96bb98e2dbef1bff
Katapangan55 <jjm121197@gmail.com>
2021-03-03 03:03:04 -0600

    getweight complete

--- main.py
+++ main.py
@@ -41,25 +41,37 @@ def getidf(term):                                  # idf = log10(N/df)
         return -1                                  # N = total num of documents
     return math.log10(len(tf_doc)/df[term])
 
+def getweight(filename, term):
+    return tf_idf_vec[filename][term]
+
+def getlen(filename):
+    return d_len[filename]
+
+def query():
+
 # Tf-Idf vector calulation - not normalized
 for filename in tf_doc:
     tf_idf_vec[filename] = Counter()
     length = 0
     for term in tf_doc[filename]:
-        weight = (1 + math.log10(tf_doc[filename][term]))*getidf(term)  # Weight formula = (1 + log10(tf_td))(idf)
-        tf_idf_vec[filename][term] = weight                             # tf_td = number occurences of term, t, in document, d
-        length += pow(weight,4)
+        tf_idf_vec[filename][term] = (1 + math.log10(tf_doc[filename][term]))*getidf(term)  # Weight formula = (1 + log10(tf_td))(idf)
+        length += tf_idf_vec[filename][term]**2       # tf_td = number occurences of term, t, in document, d
     d_len[filename] = math.sqrt(length)
+
 # Td-Idf vectors - normalized
 for filename in tf_idf_vec:
     for term in tf_idf_vec[filename]:
-        tf_idf_vec[filename][term] = tf_idf_vec[filename][term] / d_len[filename]      #dividing weights by the document's length
-
-def getweight(filename, term):
-    return tf_idf_vec[filename][term]
+        tf_idf_vec[filename][term] = tf_idf_vec[filename][term] / d_len[filename]
+        # d_len[filename] = document length
+        # tf_idf_vec[filename][term] = weight
 
 ########################################################
 # T E S T E R S
+# g e t l e n
+#print("Length of d_len for file: %d" % getlen("2012-10-03.txt"))
+#print("Length of d_len for file: %d" % getlen("1960-10-21.txt"))
+#print("Length of d_len for file: %d" % getlen("1976-10-22.txt"))
+#print("Length of d_len for file: %d" % getlen("2012-10-16.txt"))
 # g e t i d f
 print("%.12f" % getidf("health"))               # 0.079181246048
 print("%.12f" % getidf("agenda"))               # 0.363177902413
@@ -69,10 +81,15 @@ print("%.12f" % getidf("hispan"))               # 0.632023214705
 print("%.12f" % getidf("hispanic"))             # -1.000000000000
 # g e t w e i g h t
 print("\n%.12f" % getweight("2012-10-03.txt","health"))       # 0.008528366190
-print("%.12f" % getweight("1960-10-21.txt","reason"))       # 0.000000000000
-print("%.12f" % getweight("1976-10-22.txt","agenda"))       # 0.012683891289
-print("%.12f" % getweight("2012-10-16.txt","hispan"))       # 0.023489163449
-print("%.12f" % getweight("2012-10-16.txt","hispanic"))     # 0.000000000000
+print("%.12f" % getweight("1960-10-21.txt","reason"))         # 0.000000000000
+print("%.12f" % getweight("1976-10-22.txt","agenda"))         # 0.012683891289
+print("%.12f" % getweight("2012-10-16.txt","hispan"))         # 0.023489163449
+print("%.12f" % getweight("2012-10-16.txt","hispanic"))       # 0.000000000000
+# q u e r y
+print("(%s, %.12f)" % query("health insurance wall street"))        # (2012-10-03.txt, 0.033877975254)
+print("(%s, %.12f)" % query("particular constitutional amendment")) # (fetch more, 0.000000000000)
+print("(%s, %.12f)" % query("terror attack"))                       # (2004-09-30.txt, 0.026893338131)
+print("(%s, %.12f)" % query("vector entropy"))                      # (None, 0.000000000000)
 ########################################################
 # S O U R C E S
 # https://www.geeksforgeeks.org/removing-stop-words-nltk-python/
